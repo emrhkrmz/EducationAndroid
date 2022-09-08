dd40283932a4204c7d90de7e2ac065b6d4fb4b8b
emrhkrmz <kirmiziemrahcsharp@gmail.com>
2022-09-08 11:00:33 +0300

    ToggleButton

---
+++ RadioButtons/app/src/main/java/com/emrahkirmizi/radiobuttons/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.emrahkirmizi.radiobuttons
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RadioButton
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var linearLayout: LinearLayout
+    private lateinit var green: RadioButton
+    private lateinit var yellow: RadioButton
+    private lateinit var red: RadioButton
+    private lateinit var change: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        linearLayout = findViewById(R.id.linearLayout)
+        green = findViewById(R.id.radioButtonGreen)
+        yellow = findViewById(R.id.radioButtonYellow)
+        red = findViewById(R.id.radioButtonRed)
+        change = findViewById(R.id.buttonChange)
+
+        change.setOnClickListener {
+            if (green.isChecked) {
+                linearLayout.setBackgroundColor(Color.GREEN)
+            } else if (yellow.isChecked) {
+                linearLayout.setBackgroundColor(Color.YELLOW)
+            } else if(red.isChecked) {
+                linearLayout.setBackgroundColor(Color.RED)
+            }
+        }
+
+    }
+}
