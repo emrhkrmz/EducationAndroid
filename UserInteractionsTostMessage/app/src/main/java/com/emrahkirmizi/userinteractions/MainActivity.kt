26be93e864cefd5b9b26f60bd70faf54a25b90a6
emrhkrmz <kirmiziemrahcsharp@gmail.com>
2022-09-13 20:41:48 +0300

    ToastMessage

---
+++ UserInteractionsTostMessage/app/src/main/java/com/emrahkirmizi/userinteractions/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.emrahkirmizi.userinteractions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    lateinit var showToast: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        showToast = findViewById(R.id.buttonShowToast)
+
+        showToast.setOnClickListener {
+
+            Toast.makeText(applicationContext,"This is a Toast Message",Toast.LENGTH_LONG).show()
+
+        }
+    }
+}
