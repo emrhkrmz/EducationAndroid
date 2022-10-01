2248a6878777cbabc24f41d7cfb4058e450ba61f
emrhkrmz <kirmiziemrahcsharp@gmail.com>
2022-10-01 11:59:08 +0300

    AndroidManifest.xml

---
+++ BroadcastReceiver/app/src/main/java/com/emrahkirmizi/broadcastreceiver/BroadcastExample.kt
@@ -0,0 +1,12 @@
+package com.emrahkirmizi.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BroadcastExample : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+
+}
