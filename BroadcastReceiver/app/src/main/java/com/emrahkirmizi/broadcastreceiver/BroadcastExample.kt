6ba8454eb2889118006f8345a7bd9e3248f75340
emrhkrmz <kirmiziemrahcsharp@gmail.com>
2022-10-02 12:46:58 +0300

    Broadcast Receiver and  Air Plane Mode

--- BroadcastReceiver/app/src/main/java/com/emrahkirmizi/broadcastreceiver/BroadcastExample.kt
+++ BroadcastReceiver/app/src/main/java/com/emrahkirmizi/broadcastreceiver/BroadcastExample.kt
@@ -3,10 +3,23 @@ package com.emrahkirmizi.broadcastreceiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class BroadcastExample : BroadcastReceiver() {
+
     override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("Not yet implemented")
+
+        val isAirPlaneMode : Boolean = intent!!.getBooleanExtra("state",false)
+
+        if (isAirPlaneMode)
+        {
+            Toast.makeText(context,"The device is in airplane mode.",Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            Toast.makeText(context,"This device is not in airplane mode.",Toast.LENGTH_LONG).show()
+        }
+
     }
 
 }
