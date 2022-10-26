ff0c60c6c51063c3a56e5408b39703c884c7ee6e
emrhkrmz <kirmiziemrahcsharp@gmail.com>
2022-10-26 10:11:18 +0300

    ToDoList3

--- ToDoList/app/src/main/java/com/emrahkirmizi/todolist/FileHelper.kt
+++ ToDoList/app/src/main/java/com/emrahkirmizi/todolist/FileHelper.kt
@@ -1,17 +1,13 @@
 package com.emrahkirmizi.todolist
 
 import android.content.Context
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.ObjectInputStream
-import java.io.ObjectOutputStream
+import java.io.*
 
 class FileHelper {
 
-    //veri yazma yöntemi
-
     val FILENAME = "listinfo.dat"
 
+    //veri yazma yöntemi
     //birincisi argümen adı, ikincisi bağam.
     fun writeData(item : ArrayList<String>, context: Context)
     {
@@ -28,12 +24,23 @@ class FileHelper {
     {
 
         var itemList : ArrayList<String>
-        var fis : FileInputStream = context.openFileInput(FILENAME)
-        var ois = ObjectInputStream(fis)
 
-        //verileri nesne olarak okuyacak. bu yüzden onu bir dizeye dönüştürmem gerek.
-        //as ArrayList<String>
-        itemList = ois.readObject() as ArrayList<String>
+        //uygulama ilk açıldığında dosya olmasa bile boş bir dizi listesi oluşturulur ve
+        //liste görünümü boş olarak görüntülenir
+        try {
+
+            var fis : FileInputStream = context.openFileInput(FILENAME)
+            var ois = ObjectInputStream(fis)
+
+            //verileri nesne olarak okuyacak. bu yüzden onu bir dizeye dönüştürmem gerek.
+            //as ArrayList<String>
+            itemList = ois.readObject() as ArrayList<String>
+
+        }catch (e : FileNotFoundException) { //yakalama blokları
+
+            itemList = ArrayList()
+
+        }
 
         //ayrıca bu yöntem öge listesini döndürmelidir
         return itemList
--- ToDoList/app/src/main/java/com/emrahkirmizi/todolist/MainActivity.kt
+++ ToDoList/app/src/main/java/com/emrahkirmizi/todolist/MainActivity.kt
@@ -2,6 +2,7 @@ package com.emrahkirmizi.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
 
     var itemList = ArrayList<String>()
 
+    //uygulama ilk açıldığında,
+    //daha önce kaydedilmiş bir veri olup olmadığını kontrole etmem gerekiyor.
+    var fileHelper = FileHelper()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +26,27 @@ class MainActivity : AppCompatActivity() {
         item = findViewById(R.id.editText)
         add = findViewById(R.id.button)
         listView = findViewById(R.id.list)
+
+
+        //uygulama ilk açıldığında,
+        //daha önce kaydedilmiş bir veri olup olmadığını kontrole etmem gerekiyor.
+        itemList = fileHelper.readData(this)
+
+        var arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,android.R.id.text1,itemList)
+
+        //daha önce kaydedilmiş bir veri varsa bunlar ListView'a yazılır.
+        listView.adapter = arrayAdapter
+
+        //listeye veri eklemek
+        add.setOnClickListener {
+
+            var itemName : String = item.text.toString()
+            itemList.add(itemName)
+            item.setText("")
+            fileHelper.writeData(itemList,applicationContext)
+            arrayAdapter.notifyDataSetChanged()
+
+        }
 
     }
 }
