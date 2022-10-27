31fce474c59d1b9a99cb6da03b53ac13a7cfb7fd
emrhkrmz <kirmiziemrahcsharp@gmail.com>
2022-10-27 04:23:37 +0300

    ToDoListFour

--- ToDoList/app/src/main/java/com/emrahkirmizi/todolist/MainActivity.kt
+++ ToDoList/app/src/main/java/com/emrahkirmizi/todolist/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.emrahkirmizi.todolist
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -45,6 +47,32 @@ class MainActivity : AppCompatActivity() {
             item.setText("")
             fileHelper.writeData(itemList,applicationContext)
             arrayAdapter.notifyDataSetChanged()
+
+        }
+
+        listView.setOnItemClickListener { adapterView, view, position, l ->
+
+            var alert = AlertDialog.Builder(this)
+            alert.setTitle("Delete")
+            alert.setMessage("Do you want to delete this item from the list?")
+
+            //kullanıcı ekranda bir yere bastığında uyarı iletişim kutusu kapanmaz
+            alert.setCancelable(false)
+
+            alert.setNegativeButton("No", DialogInterface.OnClickListener { dialogInterface, which ->
+
+                dialogInterface.cancel()
+
+            })
+            alert.setPositiveButton("Yes", DialogInterface.OnClickListener { dialogInterface, which ->
+
+                itemList.removeAt(position)
+                arrayAdapter.notifyDataSetChanged()
+                fileHelper.writeData(itemList,applicationContext)
+
+            })
+
+            alert.create().show()
 
         }
 
