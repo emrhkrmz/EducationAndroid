1b80ec2c8baed5555cf016250f074c33bb6cd28c
emrhkrmz <kirmiziemrahcsharp@gmail.com>
2022-11-04 16:16:09 +0300

    Math Game 1

---
+++ MathGame/app/src/main/java/com/emrahkirmizitech/mathgame/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.emrahkirmizitech.mathgame
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var addition: Button
+    lateinit var subtraction: Button
+    lateinit var multi: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        addition = findViewById(R.id.buttonAdd)
+        subtraction = findViewById(R.id.buttonSub)
+        multi = findViewById(R.id.buttonMulti)
+
+        val intent = Intent (this@MainActivity,GameActivity::class.java)
+        startActivity(intent)
+
+    }
+}
